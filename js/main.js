0ea3e3f87f1b9007ad537f6ffb3fb8698a353c7a
Katrin <webentwicklerinKatrin@gmail.com>
2018-10-22 21:55:57 +0200

    stand 22.10.18

---
+++ js/main.js
@@ -0,0 +1,24 @@
+$(document).ready(function() {
+
+	//jumbotron carousel
+	$('.carousel').slick({
+	arrows: false,
+	// prevArrow:"<button type='button' class='slick-prev pull-left'><i class='fa fa-angle-left' aria-hidden='true'></i></button>",
+	// nextArrow:"<button type='button' class='slick-next pull-right'><i class='fa fa-angle-right' aria-hidden='true'></i></button>",
+	autoplay: true,
+	autoplaySpeed: 4000,
+	slidesToScroll: 1,
+	pauseOnFocus: false,
+	pauseOnHover: false,
+	draggable: true
+	});
+
+  //use these to implement prev and next click
+	$('.prev').click(function(){
+	  $('.carousel').slick('slickPrev');
+	})
+
+	$('.next').click(function(){
+	  $('.carousel').slick('slickNext');
+	})
+});
